0b6aea683f5e078afa919029065a1e924dd8aeca
DevSachin-09 <bharbeysachin@gmail.com>
2024-01-27 21:01:38 +0530

    create header section

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+'use client';
+
+import { Label, TextInput } from 'flowbite-react';
+import { HiMail } from 'react-icons/hi';
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div></div>
   )
 }
